536a489f416cacebac653ea871cf671ccb090f88
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-01-30 10:20:50 +0530

    UC 3 – On Save Create Employee Payroll Object

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -93,4 +93,53 @@ window.addEventListener('DOMContentLoaded', (event) => {
             textError.textContent = e;
         }
     });
+
+    const save = () => {
+        try {
+            let employeePayrollData = createEmployeePayroll();
+            alert("Data Saved");
+            console.log(employeePayrollData);
+        } catch (e) {
+            return;
+        }
+    };
+
+    const createEmployeePayroll = () => {
+        let employeePayrollData = new EmployeePayrollData();
+        try {
+            employeePayrollData.name = getInputValueByID('#name');
+            employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+            employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+            employeePayrollData.department = getSelectedValues('[name=department]').pop();
+            employeePayrollData.salary = getInputValueByID('#salary');
+            employeePayrollData.note = getInputValueByID('#notes');
+            employeePayrollData.startDate = getInputValueByID('#year') + "-" + getInputValueByID('#month') + "-" +
+            getInputValueByID('#day');
+            return employeePayrollData;
+        } catch (e) {
+            setTextValue('.text-error', e);
+            throw e;
+        }
+    };
+
+    const getSelectedValues = (propertyValue) => {
+        let allItems = document.querySelectorAll(propertyValue);
+        let selItems = [];
+        allItems.forEach(item => {
+            if (item.checked) selItems.push(item.value);
+        });
+        return selItems;
+    };
+
+    const getInputValueByID = (id) => {
+        let value = document.querySelector(id).value;
+        return value;
+    };
+
+    const setTextValue = (id, value) => {
+        const element = document.querySelector(id);
+        element.textContent = value;
+    };
+
+    document.querySelector('#submitButton').addEventListener('click', save);
 });
