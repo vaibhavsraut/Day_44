d219d0714c1b65f389e3ea111f39cd705c76f43d
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-01-30 10:03:26 +0530

    UC 2 – On Document Load Set Event Listeners

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -76,3 +76,21 @@ class EmployeePayrollData {
             ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
     }
 }
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+});
